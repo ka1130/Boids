87c344c002fc233626f34d9cd2cbb95a7cd2163f
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-03-09 13:13:41 +0100

    Adding first static boid.

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,20 @@
 (function() {
     document.addEventListener("DOMContentLoaded", function() {
 
-        console.log("ok");
+        const canvas = document.getElementById("canvas");
+        const ctx = canvas.getContext("2d");
+
+        const source = new Image();
+        source.src = "img/bird.svg"
+        source.width = 20;
+        source.height = 20;
+
+        source.addEventListener("load", function() {
+            ctx.drawImage(source, 0, 0);
+        });
+
+
+
 
         //end
     });
