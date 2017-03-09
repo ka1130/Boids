72602b75a04c8cde66c3a59158d5727127480ee1
Kamila Matla-Tomczyk <ka1130@o2.pl>
2017-03-09 10:53:43 +0100

    Add reset CSS.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,22 @@
 var gulp = require("gulp");
 var sass = require("gulp-sass");
+var browserSync = require("browser-sync");
 
 gulp.task("sass", function() {
     gulp.src("src/scss/main.scss")
         .pipe(sass())
-        .pipe(gulp.dest("src/css/"));
+        .pipe(gulp.dest("src/css/"))
+        .pipe(browserSync.stream());
 });
+
+gulp.task("sync", function() {
+    browserSync.init({
+        server: "src/"
+    });
+});
+
+gulp.task("watch", function() {
+    gulp.src("src/scss/**/*.scss", ["sass"]); //table with a list of tasks
+});
+
+gulp.task("default", ["sass", "sync", "watch"]);
